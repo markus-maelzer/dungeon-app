b418e6f58854e7250bdb93478544b9b841361630
Markus Mälzer <markus.maelzer@gmail.com>
2017-08-22 14:53:02 +0200

    .

--- client/src/MainReducer.js
+++ client/src/MainReducer.js
@@ -4,3 +4,25 @@ import { combineReducers } from 'redux';
 export const reducer = combineReducers({
 
 });
+
+
+const state = {
+  activeMenuItem: 'monster',
+  dataArrays: [
+    {
+      title: 'monster',
+      dataList: [
+        {
+          data: {},
+          toggleDetails: false,
+          toggleEdit: false,
+        }
+      ],
+      filterList: [],
+    },
+    {
+      title: ''
+    }
+  ],
+
+}
