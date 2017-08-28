bd7b7fda78d4bb92a49def73fbe1d056b49f3e32
Markus Mälzer <markus.maelzer@gmail.com>
2017-08-28 09:00:27 +0200

    finished getServerData function

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,68 +1,28 @@
 import axios from 'axios';
 
-/*
-export const postFilepath = (path) => dispatch => fetch('api/filepath', {
-  method: 'post',
-  body: JSON.stringify({
-    filePath: path
-  }),
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-}).then(checkStatus)
-*/
-export const getServerData = (filepath) => {
+export const getServerData = filepath => {
   return dispatch =>  {
     return dispatch({
       type: 'GET_SERVER_DATA',
       payload: {
         promise: new Promise((resolve, rejected) => {
-          axios.get('api/dungeon')
+          axios.post('api/filepath', {
+            filepath: filepath
+          })
+          .then(response => {
+            axios.get('api/dungeon')
             .then(response => {
-              resolve(response.data);
+              resolve(response.data)
             })
             .catch(error => {
-              rejected(error.data)
+              rejected(error.data);
             })
+          })
+          .catch(error => {
+            rejected(error.data);
+          });
         }),
       }
     })
   }
 }
-
-
-
-/*
-promise: new Promise((resolve, rejected) => {
-  fetch('api/dungeon', {
-    header: {
-      Accept: 'application/json',
-    },
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error(response);
-    }
-    resolve(response.json());
-  }).catch(error => {
-    rejected(error);
-  })
-}),
-
-
-const checkStatus = (response) => {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  } else {
-    const error = new Error('HTTP Error: ' + response.statusText);
-    error.status = response.statusText;
-    error.response = response;
-    console.log(error);
-    throw error;
-  }
-}
-
-function parseJSON(response) {
-  return response.json()
-}
-*/
