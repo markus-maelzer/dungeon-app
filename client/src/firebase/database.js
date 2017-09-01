1ccf91781ca94f52e950283d13b1e01cf0b18355
Markus Mälzer <markus.maelzer@gmail.com>
2017-09-01 13:18:58 +0200

    data base config

---
+++ client/src/firebase/database.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyANuSK-pRx0Tl5ypmt7a-oJ6v-gxiJY4j0",
+  authDomain: "dungeon-app-d8e01.firebaseapp.com",
+  databaseURL: "https://dungeon-app-d8e01.firebaseio.com",
+  projectId: "dungeon-app-d8e01",
+  storageBucket: "dungeon-app-d8e01.appspot.com",
+  messagingSenderId: "827489957091"
+};
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
