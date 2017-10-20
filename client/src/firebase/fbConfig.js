62c0c8b5ef6979285f4dd9f389485e83fa66b0c5
Markus Mälzer <markus.maelzer@gmail.com>
2017-10-20 13:18:29 +0200

    changed name from database.js

---
+++ client/src/firebase/fbConfig.js
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyANuSK-pRx0Tl5ypmt7a-oJ6v-gxiJY4j0",
+  authDomain: "dungeon-app-d8e01.firebaseapp.com",
+  databaseURL: "https://dungeon-app-d8e01.firebaseio.com",
+  projectId: "dungeon-app-d8e01",
+  storageBucket: "dungeon-app-d8e01.appspot.com",
+  messagingSenderId: "827489957091"
+};
+
+export const config = {
+  userProfile: 'users',
+  enableLogging: false,
+}
+
+/*
+firebase.initializeApp(config);
+database = firebase.database();
+*/
